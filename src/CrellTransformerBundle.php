fb0bcc77677418b83a479ce2864c776342950f52
Larry Garfield <larry@garfieldtech.com>
2015-01-11 20:56:35 -0600

    Add file header docblocks.

--- src/CrellTransformerBundle.php
+++ src/CrellTransformerBundle.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This file is part of the Transformer Bundle library.
+ *
+ * (c) Larry Garfield <larry@garfieldtech.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Crell\Transformer
+ */
+
 namespace Crell\TransformerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
